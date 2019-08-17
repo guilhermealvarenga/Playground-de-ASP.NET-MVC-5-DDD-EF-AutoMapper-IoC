84f5e0598504e5b559e2c3701b84bec008c28802
Guilherme de Alvarenga <gui.alva50@gmail.com>
2019-08-17 01:55:58 -0300

    Configurando Repositório Genérico

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Domain/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ProjetoModeloDDD.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        TEntity GetById(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> GetAllAsNoTraking();
+        void Update(TEntity obj);
+
+        void Remove(TEntity obj);
+        void Dispose();
+    }
+}
---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,52 @@
+﻿using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected ProjetoModeloContext Db = new ProjetoModeloContext();
+        public void Add(TEntity obj)
+        {
+            // TODO: Implementar padrão Unit Of Work.
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public IEnumerable<TEntity> GetAllAsNoTraking()
+        {
+            return Db.Set<TEntity>().AsNoTracking().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+    }
+}
