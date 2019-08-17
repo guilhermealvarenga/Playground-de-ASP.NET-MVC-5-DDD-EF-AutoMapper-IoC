bdef8b98ddd463428b2b3b32ed4264814f938c91
Guilherme de Alvarenga <gui.alva50@gmail.com>
2019-08-17 01:16:17 -0300

    Fluent API
    Para mapear comportamento da minha entidade de domínio Cliente para ser modelado como uma tabela no banco dados

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/EntityConfig/ClienteConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoModeloDDD.Infra.Data.EntityConfig
+{
+    /// <summary>
+    /// Fluent API.
+    /// Para mapear comportamento da minha entidade de domínio Cliente para ser modelado como uma tabela no banco dados.
+    /// </summary>
+    public class ClienteConfiguration : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteConfiguration()
+        {
+            HasKey(c => c.ClienteId);
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(c => c.Sobrenome)
+               .IsRequired()
+               .HasMaxLength(150);
+
+            Property(c => c.Email)
+               .IsRequired();
+        }
+    }
+}
